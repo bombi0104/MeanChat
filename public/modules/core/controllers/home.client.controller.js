c8e40e357e48d64c358bdd2cc7ce05a91266dc5d
taipa <taipa@nec.vn>
2015-06-17 16:42:16 +0700

    Init : select first group.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -9,11 +9,42 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		$scope.groups = [];
 		$scope.group = undefined;
 
+
+		/**
+		 *
+		 */
+		var updateActiveGroup = function(gr){
+			$scope.groups.forEach(function(group){
+				if (group._id === gr._id) {
+					group.active = true;
+				} else {
+					group.active = false;
+				}
+			});
+		};
+
+		/**
+		 *
+		 */
+		$scope.selectGroup = function(gr){
+			$scope.group = gr;
+			console.debug('%j',gr);
+			updateActiveGroup(gr);
+
+		};
+
 		var init = function(){
 			$scope.groups = Groups.query();
-			console.log($scope.groups);
-		}
+			$scope.groups.$promise.then(function (result) {
+    			$scope.selectGroup(result[0]);
+			});
+			$scope.selectGroup($scope.groups[0]);
+		};
+
+		
 
 		init();
+
+		
 	}
 ]);
